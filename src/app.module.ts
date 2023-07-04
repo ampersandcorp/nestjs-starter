a41a30764e60b33359508a8ed22eb5c6baffb5b9
stevejkang <iam@juneyoung.io>
2023-07-04 19:19:23 +0900

    Update import order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
