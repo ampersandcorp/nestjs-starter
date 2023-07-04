530e2405ba9b5f8f3023ad82e8107e0ab4a05660
stevejkang <iam@juneyoung.io>
2023-07-04 19:18:27 +0900

    Fix eslint issue
    - Remove magic number from nestjs application port number definition

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  const port = 80;
+
+  await app.listen(port);
 }
 bootstrap();
